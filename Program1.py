61f1ef4150c47ae017dac5f5099c7e96727d27f5
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-12-05 14:56:38 +0800

    PROGRAM 1 MODIFIED: After deliberating many functionalities I came up with a concept. I removed for loop and replace def and while for the yes or no interface.

--- Program1.py
+++ Program1.py
@@ -1,3 +1,5 @@
+import random 
+# ----------- CONTEXT ---------------
 # Program 1: Lottery
 # Create a program that ask 3 numbers (0-9) from the user.
 # Generate 3 random winning numbers (0-9)
@@ -7,24 +9,36 @@
 # If the user enter “y” the user will play again
 # if “n” the program will exit.
 
-"""
-In this program, I will use random module.
-"""
-import random
+def get_randWinNum():
+    randWinNum1 = random.randint(0,9)
+    randWinNum2 = random.randint(0,9)
+    randWinNum3 = random.randint(0,9)
+    return [randWinNum1, randWinNum2, randWinNum3]
 
-def winning_num():
-    winningNum = []
-    for i in range(3):
-        winningN = random.randint(0,9)
-        winningNum.append(winningN)
-    return winningNum
-def user_num():
-    userInput = []
-    for i in range(3):
-        userNums = int(input("Please enter a number: "))
-        userInput.append(userNums)
-    return userInput
+def get_userNums():
+    userNum1 = input("\nEnter your first entry number: ")
+    userNum2 = input("Enter your second entry number: ") 
+    userNum3 = input("Enter your third entry number: ")
+    return [int(userNum1), int(userNum2), int(userNum3)]
 
-winNum = winning_num()
-userNum = user_num()
-print(f"Your number is {userNum}\nThe winning number is {winNum}\n")
+userInterface = "try again"
+repeatX = "y"
+while userInterface == "try again":
+    userNums = get_userNums()
+    randWinNums = get_randWinNum()
+    userInterface = "_"
+    while repeatX == "y":
+        repeatX = "_"
+        if userNums == randWinNums:
+            print(f"\nWinner!\nYou've got the winning numbers! {randWinNums}\nYour Numbers: {userNums}")
+        else:
+            print(f"\nYou loss.\nThe winning numbers are {randWinNums}\nYour Numbers: {userNums}")
+        print("\nDo you wish to try again?\n[y/n]")
+    while userInterface == "_":
+        repeatX = input("\n> ").lower()
+        if repeatX == "n":
+            userInterface = "exit the program"
+        elif repeatX == "y":
+            userInterface = "try again"
+        else:
+            print("We're sorry. The lottery system can't understand your input.\nDo you wish to try again?[y/n]\n[y] for Yes or [n] for No.\n")
