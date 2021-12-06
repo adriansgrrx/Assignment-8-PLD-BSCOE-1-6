0250d3cb7f21a57fa554a0cdb746f8fae23cd100
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-12-06 21:51:32 +0800

    PROGRAM 2 MODIFIED: Completed but I changed some print statements and add some message to the user for infos.

--- Program2.py
+++ Program2.py
@@ -23,15 +23,23 @@ while randNum != "user guess":
         userGuess = int(input("\nGuess a number ranging from 0 to 100: "))  #userGuess variable = simply the user's guess
         if int(userGuess) != int(randNum):  # condition if the user guess didn't match the random number
             if int(userGuess) > int(randNum): # if need higher number
-                print("[Incorrect]\nYou need to guess LESS THAN your previous number.")
+                print("[Incorrect]\nHINT: Greater than")
             elif int(userGuess) < int(randNum): # if need lower number
-                print("[Incorrect]\nYou need to guess GREATER THAN your previous number.")
+                print("[Incorrect]\nHINT: Less than")
         elif int(userGuess) == int(randNum): # condition if the user guess matched the random number
             randNum = "user guess"
+        
+        if int(userGuess) > 100:
+            print("Please try again. Keep in mind that the system range is from 0 to 100 ONLY.")
+            continue;
+        elif int(userGuess) < 0:
+            print("Please try again. Keep in mind that the system range is from 0 to 100 ONLY.")
+            continue;
+            
     # I used the except functionality for error validation specifically ValueError and EOFError 
     # just to be sure that the program wont exit resuting from any error inputs
     except ValueError or EOFError:
-        print("Invalid Input")
+        print("[Invalid Input]\nThe system only accept INTEGERS. Please do not include any other characters.\nPlease try again.")
 else:
     #Display the result after guessing the number correctly
-    print(f"\nCongratulations!\nYou successfully guessed the mystery number!\nYour guess: {userGuess}\n")
+    print(f"\nCongratulations!\nYou successfully guessed the mystery number!\nMystery Number: {userGuess}\n")
