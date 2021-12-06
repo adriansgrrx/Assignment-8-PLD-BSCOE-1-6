54720e9f41ca328ec760d15b45631c46cf7f0b61
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-12-06 16:19:13 +0800

    PROGRAM 2 COMPLETE: I already put while condition to satisfy the loop. All in all, the program is already completed, and the all the requirements are already satisfied. But, I will try to include error validations and if-elif statements for the given range.

--- Program2.py
+++ Program2.py
@@ -10,8 +10,21 @@ This program will use randrange module from random library.
 """
 from random import randrange
 
-# generate number from 1-100
-randNum = randrange(0,100)
-# ask the user for a guess
-userGuess = input("Guess a number ranging from 0 to 100: ")
+# Greetings
+print("\nWelcome to Guess the Mystery Number!")
 
+# generate a number from 1-100
+randNum = randrange(0,100)  #randNum variable = random number
+# ask the user for a guess
+userGuess = int(input("\nGuess a number ranging from 0 to 100: "))  #userGuess variable = simply the user's guess
+
+# while condition that must satisfy
+while userGuess != randNum:
+    if userGuess > randNum:
+        print("[Incorrect]\nYou need to guess LESS THAN your previous number.")
+        userGuess = int(input("\nGuess a number ranging from 0 to 100: "))
+    else:
+        print("[Incorrect]\nYou need to guess GREATER THAN your previous number.")
+        userGuess = int(input("\nGuess a number ranging from 0 to 100: "))
+#Display the result after guessing the number correctly
+print(f"\nYou successfully guessed the mystery number\nMystery Number: {randNum}\nYour guess: {userGuess}\nCongratulations!\n")
