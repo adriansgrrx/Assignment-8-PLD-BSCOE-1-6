98304368ab5e07b4d15d3aa8c0270077bb64a40e
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-12-06 20:54:34 +0800

    PROGRAM 2 MODIFIED: I add validation error and corrected some typos.

--- Program2.py
+++ Program2.py
@@ -18,17 +18,20 @@ randNum = randrange(0,100)  #randNum variable = random number
 
 # while condition that must satisfy
 while randNum != "user guess":
-    # ask the user for a guess
-    userGuess = int(input("\nGuess a number ranging from 0 to 100: "))  #userGuess variable = simply the user's guess
-    if int(userGuess) != int(randNum):  # condition if the user guess didn't match the random number
-        if int(userGuess) > int(randNum): # if need higher number
-            print("[Incorrect]\nYou need to guess LESS THAN your previous number.")
-
-        elif int(userGuess) < int(randNum): # if need lower number
-            print("[Incorrect]\nYou need to guess GREATER THAN your previous number.")
-
-    elif int(userGuess) == int(randNum): # condition if the user guess matched the random number
-        randNum = "user guess"
+    try:
+        # ask the user for a guess
+        userGuess = int(input("\nGuess a number ranging from 0 to 100: "))  #userGuess variable = simply the user's guess
+        if int(userGuess) != int(randNum):  # condition if the user guess didn't match the random number
+            if int(userGuess) > int(randNum): # if need higher number
+                print("[Incorrect]\nYou need to guess LESS THAN your previous number.")
+            elif int(userGuess) < int(randNum): # if need lower number
+                print("[Incorrect]\nYou need to guess GREATER THAN your previous number.")
+        elif int(userGuess) == int(randNum): # condition if the user guess matched the random number
+            randNum = "user guess"
+    # I used the except functionality for error validation specifically ValueError and EOFError 
+    # just to be sure that the program wont exit resuting from any error inputs
+    except ValueError or EOFError:
+        print("Invalid Input")
 else:
     #Display the result after guessing the number correctly
-    print(f"\nCongratulations!\nYou successfully guessed the mystery number!\nnYour guess: {userGuess}\n")
+    print(f"\nCongratulations!\nYou successfully guessed the mystery number!\nYour guess: {userGuess}\n")
